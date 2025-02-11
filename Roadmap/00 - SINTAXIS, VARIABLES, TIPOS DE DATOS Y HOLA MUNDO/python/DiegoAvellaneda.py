f88077d8e0f4046a2c4a05c8b87b9d0351f60197
Diego Avellaneda <daacdiego2015@gmail.com>
2025-02-11 08:21:54 -0500

    Solucion #0

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DiegoAvellaneda.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+
+# Comentario 1 linea
+"""
+Comentario de varias lineas
+
+"""
+
+"""
+Otro tipo de comentario con comillas sencillas
+"""
+
+variable=""
+CONSTANTE=3.14159
+
+texto="String"
+numero=8
+verdad=True
+
+print(type(variable))
+print(type(CONSTANTE))
+print(type(texto))
+print(type(numero))
+print(type(verdad))
+
+print("¡Hola, Python!")
